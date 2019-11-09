8049b5b48fabbd4968dcc29f6f219b8620534442
Cezar Takada <cezar.takada@gmail.com>
2019-11-09 14:20:25 -0300

    agregada la ruta mensages

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function (req,res){
+app.use(router);
+
+router.get('/message', function (req, res){
+    res.send('lista de mensajes');
+});
+
+router.post('/message',function (req,res){
+    res.send('mensaje agregado');
+});
+
+/* app.use('/', function (req,res){
     res.send('Hola');
-})
+}) */
 
 app.listen(3000);
 console.log('la aplicación está escuchando en http://localhost:3000');
